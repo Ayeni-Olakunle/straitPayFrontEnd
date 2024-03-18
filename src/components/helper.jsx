113cb1ed215c55e6b3a56a38cd637c70c3651d5c
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-03-18 11:07:10 +0100

    update

--- src/components/helper.jsx
+++ src/components/helper.jsx
@@ -3,5 +3,5 @@ export const timeStampDate = (created_at) => {
     const month = created_at.slice(5, 7);
     const day = created_at.slice(8, 10);
 
-    return `${year}-${month}-${day}/}`
+    return `${year}-${month}-${day}}`
 }
