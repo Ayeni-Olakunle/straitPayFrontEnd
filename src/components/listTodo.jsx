9e6718fd8b5cc67ebaf4e352f3011cd5c9a14e01
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-03-18 11:26:49 +0100

    update

--- src/components/listTodo.jsx
+++ src/components/listTodo.jsx
@@ -211,11 +211,11 @@ function ListTodo() {
             <div className="flex justify-between items-center gap-[50px] md:flex-col md:gap-0">
                 <div className="w-full flex justify-start items-start flex-col mx-[0] my-[10px]">
                     <label htmlFor="createdAT" className="text-[#818181] text-lg w-full outline-none">Created Date</label>
-                    <input type="date" id="createdAT" name="createdAT" defaultValue={createAt} placeholder="Create" className="border-[1.4px] border-solid border-[#afafaf] w-full p-[10px] text-[17px] rounded-[5px] mx-[0] my-[5px] outline-none bg-[#F6F6F6] text-[#606060]" disabled />
+                    <input type="date" id="createdAT" name="createdAT" defaultValue={createAt} placeholder="Create" className="border-[1.4px] md:w-full border-solid border-[#afafaf] w-full p-[10px] text-[17px] rounded-[5px] mx-[0] my-[5px] outline-none bg-[#F6F6F6] text-[#606060]" disabled />
                 </div>
                 <div className="w-full flex justify-start items-start flex-col mx-[0] my-[10px]">
                     <label htmlFor="update" className="text-[#818181] text-lg w-full outline-none">Update Date</label>
-                    <input type="date" id="update" name="update" placeholder="update" defaultValue={updatedAt} className="border-[1.4px] border-solid border-[#afafaf] w-full p-[10px] text-[17px] rounded-[5px] mx-[0] my-[5px] outline-none bg-[#F6F6F6] text-[#606060]" disabled />
+                    <input type="date" id="update" name="update" placeholder="update" defaultValue={updatedAt} className="border-[1.4px] md:w-full border-solid border-[#afafaf] w-full p-[10px] text-[17px] rounded-[5px] mx-[0] my-[5px] outline-none bg-[#F6F6F6] text-[#606060]" disabled />
                 </div>
             </div>
             <div className="w-full flex justify-start items-start flex-col mx-[0] my-[10px]">
